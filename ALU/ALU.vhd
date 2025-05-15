76b6d7489f08caa3dc63460de16a5778c45a9e01
Adryan Castro <adryan.castro123@hotmail.com>
2025-05-15 17:01:22 -0300

    Refining
    - Adição de mais comentários
    - Melhoria em relação a repetição de código
    - Nomeando melhor as variáveis
    - Alteração do nome do sinal de saída pois output pode vir a ser uma palavra reservada

--- ALU/ALU.vhd
+++ ALU/ALU.vhd
@@ -13,33 +13,38 @@ entity ALU is
         input_0, input_1 : in unsigned (15 downto 0);
         operation_selector: in unsigned (1 downto 0);
         carry, greater_equal, less_equal: out std_logic; 
-        output : out unsigned (15 downto 0)
+        saida : out unsigned (15 downto 0)
     );
 end entity;
 
 architecture ALU_arch of ALU is
 
+    -- Sinais auxiliares
     signal input_0_17bits, input_1_17bits, sum_result, sub_result : unsigned(16 downto 0);
 
 begin
-    -- Formando 17 bits para armezenar o carry junto com o resultado da operação
+    -- [*] Melhoria Futura: Tratamento com números negativos, ou seja, não usar mais unsigned e sim como signed
+    input_0 <= unsigned(input_0);
+    input_1 <= unsigned(input_1);
+
+    -- Formando 17 bits para armazenar o carry junto com o resultado da operação
     input_0_17bits <= '0' & input_0;
     input_1_17bits <= '0' & input_1;
 
-    sum_result <= input_0_17bits + input_1_17bits;
-    sub_result <= input_0_17bits - input_1_17bits;
+    sum_result_17bits <= input_0_17bits + input_1_17bits; -- Carry
+    sub_result_17bits <= input_0_17bits - input_1_17bits; -- Borrow
 
-    output <= unsigned(input_0) + unsigned(input_1) when operation_selector = "00" else
-              unsigned(input_0) - unsigned(input_1) when operation_selector = "01" else
-              unsigned(input_0) and unsigned(input_1) when operation_selector = "10" else
-              unsigned(input_0) xor unsigned(input_1) when operation_selector = "11" else
+    saida <= input_0 + input_1 when operation_selector = "00" else
+              input_0 - input_1 when operation_selector = "01" else
+              input_0 and input_1 when operation_selector = "10" else
+              input_0 xor input_1 when operation_selector = "11" else
               "0000000000000000";
 
-    carry <= sum_result(16) when operation_selector = "00" else 
-             sub_result(16) when operation_selector = "01" else
+    carry <= sum_result_17bits(16) when operation_selector = "00" else
+             sub_result_17bits(16) when operation_selector = "01" else
              '0';
 
-    greater_equal <= '1' when unsigned(input_0) >= unsigned(input_1) else '0';
-    less_equal <= '1' when unsigned(input_0) <= unsigned(input_1) else '0';
+    greater_equal <= '1' when input_0 >= input_1 else '0';
+    less_equal <= '1' when input_0 <= input_1 else '0';
 
 end architecture;
