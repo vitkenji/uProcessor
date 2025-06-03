2c98f1895dbf8e102530b0d0bac85adccd769a2e
vitkenji <vkz.yamada@gmail.com>
2025-06-03 16:24:55 -0300

    Máquina de estados

--- State_Machine/State_Machine.vhd
+++ State_Machine/State_Machine.vhd
@@ -4,12 +4,22 @@ use ieee.numeric_std.all;
 
 entity State_Machine is
     port(
-
+        clk : in std_logic;
+        rst : in std_logic;
+        state : out std_logic
     );
 end entity;
 
 architecture State_Machine_arch of State_Machine is
-    process
+    signal state_s: std_logic := '0';
+begin
+    process(clk, rst)
     begin
+        if rst = '1' then
+            state_s <= '0';
+        elsif rising_edge(clk) then 
+                state_s <= not state_s;
+        end if;
     end process;
+    state <= state_s;
 end architecture;
--- State_Machine/State_Machine_tb.vhd
+++ State_Machine/State_Machine_tb.vhd
@@ -6,8 +6,56 @@ entity State_Machine_tb is
 end;
 
 architecture State_Machine_tb_arch of State_Machine_tb is
+    component State_Machine
+    port (
+        clk : in std_logic;
+        rst : in std_logic;
+        state : out std_logic
+    );
+    end component;
+
+    constant period_time : time := 100 ns;
+    signal finished : std_logic := '0';
+    signal clk , reset, rst : std_logic;
+    signal state : std_logic;
+
+begin
+    uut : State_Machine port map (
+        clk => clk,
+        rst => rst,
+        state => state
+    );
+
+    reset_global : process
+    begin
+        reset <= '1';
+        wait for period_time * 2;
+        reset <= '0';
+        wait;
+    end process;
+
+    sim_time_proc : process
+    begin
+        wait for 10 us;
+        finished <= '1';
+        wait;
+    end process sim_time_proc; 
+    
+    clk_proc : process
+    begin
+        while finished /= '1' loop
+            clk <= '0';
+            wait for period_time / 2;
+            clk <= '1';
+            wait for period_time / 2;
+        end loop;
+        wait;
+    end process clk_proc;
+
     process
     begin
+        wait for 200 ns;
+        wait;
     end process;
 end architecture;
 
