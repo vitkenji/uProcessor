26ccb7252da77c742de73fc77de8fe5da49fa992
vitkenji <vkz.yamada@gmail.com>
2025-06-03 23:06:02 -0300

    Control Unit funcionando para jump_enable

--- Control_Unit/Control_Unit.vhd
+++ Control_Unit/Control_Unit.vhd
@@ -4,9 +4,35 @@ use ieee.numeric_std.all;
 
 entity Control_Unit is
     port(
-
+        instruction : in unsigned (16 downto 0);
+        clk : in std_logic;
+        rst : in std_logic;
+        jump_enable : out std_logic
     );
 end entity;
 
 architecture Control_Unit_arch of Control_Unit is
+    component State_Machine
+    port(
+        clk : in std_logic;
+        rst : in std_logic;
+        state : out std_logic
+    );
+    end component;
+
+    signal opcode : unsigned (3 downto 0);
+    signal state_s : std_logic;
+
+    begin
+
+        uut_state_machine : State_Machine port map (
+            clk => clk,
+            rst => rst,
+            state => state_s
+        );
+
+        opcode <= instruction (3 downto 0);
+        jump_enable <= '1' when state_s = '1' and opcode = "1111" else
+                       '0'; 
+        
 end architecture;
--- Control_Unit/Control_Unit_tb.vhd
+++ Control_Unit/Control_Unit_tb.vhd
@@ -6,5 +6,61 @@ entity Control_Unit_tb is
 end;
 
 architecture Control_Unit_tb_arch of Control_Unit_tb is
+    component Control_Unit
+        port(
+                instruction : in unsigned (16 downto 0);
+                clk : in std_logic;
+                rst : in std_logic;
+                jump_enable : out std_logic
+            );
+    end component;
+
+    constant period_time : time := 100 ns;
+    signal finished : std_logic := '0';
+    signal clk , reset, rst : std_logic;
+
+    signal instruction : unsigned (16 downto 0);
+    signal jump_enable : std_logic;
+
+    begin
+        uut : Control_Unit port map(
+            clk => clk,
+            rst => rst,
+            instruction => instruction,
+            jump_enable => jump_enable
+        );
+        
+        reset_global : process
+    begin
+        reset <= '1';
+        wait for period_time * 2;
+        reset <= '0';
+        wait;
+    end process;
+
+    sim_time_proc : process
+    begin
+        wait for 10 us;
+        finished <= '1';
+        wait;
+    end process sim_time_proc; 
     
+    clk_proc : process
+    begin
+        while finished /= '1' loop
+            clk <= '0';
+            wait for period_time / 2;
+            clk <= '1';
+            wait for period_time / 2;
+        end loop;
+        wait;
+    end process clk_proc;
+
+    process
+    begin
+        instruction <= "00000000000011110";
+        wait for 200 ns;
+        instruction <= "00000000000011111";
+        wait;
+    end process;
 end architecture;
